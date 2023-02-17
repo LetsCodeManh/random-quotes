3633a77947a30def03db61e96e3d8d34069bd0d0
LetsCodeManh <manh.ng@icloud.com>
2023-02-17 18:39:28 +0100

    feat: Set up the Project

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import { Inter } from '@next/font/google'
+import Content from "@/components/Content";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import { Inter } from "@next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
-     Hello
-    </main>
-  )
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
 }
